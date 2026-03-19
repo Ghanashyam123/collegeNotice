89730fe87e5963c841cf462c020a3d6a15e5d052
Ghanashyam123 <adshyam68@gmail.com>
2026-03-19 08:43:41 +0545

    exam important

---
+++ examVVI/session/session_create.php
@@ -0,0 +1,18 @@
+<?php 
+session_start();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Session example</title>
+</head>
+<body>
+    <form method="post" action="">
+
+        <input type="text" name="username"/>
+        <input type=submit name="submit"/>
+    </form>
+</body>
+</html>
